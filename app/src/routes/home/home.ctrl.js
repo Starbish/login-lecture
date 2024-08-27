31753cb1a29481d936ecc9d6920f85886f2933b7
Starbish <starbish96@naver.com>
2024-08-27 15:21:27 +0900

    login

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,43 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
-}
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
 
-const login = (req, res) => {
-    res.render("home/login");
+    login: (req, res) => {
+        res.render("home/login");
+    },
+};
+
+const users = {
+    id: ["woorimIT", "abc", "def"],
+    psword: ["1234", "1234", "123456"],
+};
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        let idx = users.id.indexOf(id);
+        if( idx != -1 ) {
+            console.log(idx);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        
+        return res.json({
+            success: false,
+            msg: "Failed to sign in",
+        });
+    },
 }
 
 module.exports = {
 
-    home,
-    login,
+    output, process,
 };
